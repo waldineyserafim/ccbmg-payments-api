54760c4276c297b015da517e631ee53b59fb69df
W. Serafim <30632997+waldineyserafim@users.noreply.github.com>
2025-09-29 14:37:33 -0300

    Update _mp.js

--- api/_mp.js
+++ api/_mp.js
@@ -1,1 +1,9 @@
+import { MercadoPagoConfig, Preference, Payment } from "mercadopago";
+
+export const mpClient = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN
+});
+
+export const mpPreference = new Preference(mpClient);
+export const mpPayment = new Payment(mpClient);
 
